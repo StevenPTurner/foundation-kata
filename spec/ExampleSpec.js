8f80b3e6c0338f57fc16865c7f96b68b3a8b5b25
Steven Turner <steven.turner@verint.com>
2020-06-04 10:42:05 +0100

    added extra test

--- spec/ExampleSpec.js
+++ spec/ExampleSpec.js
@@ -1,13 +1,16 @@
-describe('example', function(){
+describe('example', function () {
     var example;
 
-    beforeEach(function(){
+    beforeEach(function () {
         example = require('../src/Example.js');
     });
 
-    describe('addTwoNumbers', function() {
-        it('should calculate 4 when 2 and 2 are passed in', function(){
-            expect(example.add(2,2)).toBe(4);
+    describe('addTwoNumbers', function () {
+        it('should calculate 4 when 2 and 2 are passed in', function () {
+            expect(example.add(2, 2)).toBe(4);
+        });
+        it('should calculate 3 when 2 and 1 are passed in', function () {
+            expect(example.add(2, 1)).toBe(3);
         });
     });
 });
